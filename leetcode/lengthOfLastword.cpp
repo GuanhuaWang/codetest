9b7dae70f118357e6f4a348945ffd4269d8747a2
GuanhuaWang <alexwgh333@gmail.com>
2017-09-10 23:14:54 -0700

    modify length of last word

--- leetcode/lengthOfLastword.cpp
+++ leetcode/lengthOfLastword.cpp
@@ -1,39 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int lengthOfLastWord(char* s) {
+
     int len = strlen(s);
-    int count=0;
-    int flg =0;
-    int blank =0;
-    for(int k = 0;k<len;k++)
-    {
-        if(s[k]==' ')
-        {
-            blank++;
-            flg = 1;
-        }
-    }
-    if(blank==len) return 0;
-    if(blank ==1 && s[len-1]==' ') return len-1;
-    if(flg==0)
-        return len;
-    
-    for(int i = 0;i<len;i++)
-    {
-        if(s[i]==' ')
-        {
-            count=0;
-            for(int j =i+1;j<len;j++)
-            {
-                if(s[j]!=' ')
-                    count++;
-                else
-                {
-                    break;    
-                }
-            }
-        }
-    }
-    if(len==0)
+    if (len == 0) return 0;
+    int i = len -1;
+    while(s[i] ==' ' && i>=0) i--;
+    if(i == -1)
         return 0;
-    else
-        return count;
+    
+    int end = i;
+    for(;i>=0;i--)
+    {
+        if(s[i] == ' ')
+            break;
+    }
+    if(i == -1)
+        return end+1;
+    return end-i;
+}
+
+int main(int argc, char const *argv[])
+{
+    char c[]={"  aaa     "};
+    int a = lengthOfLastWord(c);
+    printf("a is %d\n", a);
+    return 0;
 }
