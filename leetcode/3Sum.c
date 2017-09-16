c7b038975d971ba10bf96f3be54c14faadfef586
GuanhuaWang <alexwgh333@gmail.com>
2017-09-16 12:46:44 -0700

    add 3Sum

--- leetcode/3Sum.c
+++ leetcode/3Sum.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * Return an array of arrays of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
@@ -25,36 +27,7 @@ void quickSort(int* nums, int first, int last){
     quickSort(nums,first,left-1);
     quickSort(nums,left+1,last);
 }
-/*
-int** threeSum(int* nums, int numsSize, int* returnSize) {  
-    int i,sum,top=-1,begin,end;  
-    int** res=(int**)malloc(sizeof(int*)*(numsSize*(numsSize-1)*(numsSize-2))/6);  
-    if(numsSize<3){  
-        *returnSize=0;  
-        return res;  
-    }  
-    quickSort(nums,0,numsSize-1);  
-    for(i=0;i<numsSize;i++){  
-        if(nums[i]>0)break;  
-        if(i>0 && nums[i]==nums[i-1])continue;  
-        begin=i+1;end=numsSize-1;  
-        while(begin<end){  
-            sum=nums[i]+nums[begin]+nums[end];  
-            if(sum==0){  
-                top++;  
-                res[top]=(int*)malloc(sizeof(int)*3);  
-                res[top][0]=nums[i];res[top][1]=nums[begin];res[top][2]=nums[end];  
-                begin++;end--;  
-                while(begin<end && nums[begin]==nums[begin-1])begin++;  
-                while(begin<end && nums[end]==nums[end+1])end--;  
-            }else if(sum>0) end--;  
-            else begin++;  
-        }  
-    }  
-    *returnSize=top+1;  
-    return res;  
-}  
-*/
+
 
 int** threeSum(int* nums, int numsSize, int* returnSize) {
     int sum,top = -1;
@@ -101,3 +74,4 @@ int** threeSum(int* nums, int numsSize, int* returnSize) {
     *returnSize = top+1;
     return final;
 }
+
