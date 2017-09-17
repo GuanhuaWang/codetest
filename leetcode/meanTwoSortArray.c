0c33a6579c67e6329f8e2103d8849790b07620a3
GuanhuaWang <alexwgh333@gmail.com>
2017-09-16 20:45:15 -0700

    add meanTwoSortArray

---
+++ leetcode/meanTwoSortArray.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    int count1=0;
+    int count2=0;
+
+
+    if(nums1Size+nums2Size==1)
+    {
+        if(nums1Size == 1) return nums1[0];
+        else return nums2[0];
+    }
+    for(int i = 0;i<nums1Size;i++)
+    {
+        for(int j = 0; j<nums2Size;j++)
+        {
+            //printf("count1 is %d, count2 is %d\n", count1,count2);
+            if(nums2[j]>=nums1[i])
+                count1++;
+            else
+                count2++;
+            
+            if(nums1Size+nums2Size==3 && count1+count2==1)
+            {
+                if(count1>count2)
+                    return nums2[0];
+                else 
+                    return nums1[0];
+            }
+            else if(count1+count2==(nums1Size+nums2Size)/2-1)
+            {   
+                //printf("count1 is %d, count2 is %d\n", count1,count2);
+                break;
+            }
+        }
+        if(count1+count2==(nums1Size+nums2Size)/2-1)
+            break;
+    }
+   // printf("count1 is %d, count2 is %d\n", count1,count2);
+   // printf("data is %d, %d \n",nums1[count1],nums2[count2-1]);
+    return (nums1[count1]+nums2[count2])/2.0;
+}
+
+int main(int argc, char const *argv[])
+{
+    
+    int a1[] = {};
+    int a2[] ={2};
+
+    double c = findMedianSortedArrays(a1,0,a2,1);
+    printf("the mean is %f\n", c);
+    return 0;
+}
