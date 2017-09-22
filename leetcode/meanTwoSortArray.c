9e320126cc31c5fe1d12749c1cbd3c38ed6c4a62
GuanhuaWang <alexwgh333@gmail.com>
2017-09-22 10:48:38 -0700

    modify meanTwoSortArray

--- leetcode/meanTwoSortArray.c
+++ leetcode/meanTwoSortArray.c
@@ -2,60 +2,34 @@
 #include <stdlib.h>
 
 double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
-    int count1=0;
-    int count2=0;
+    if((nums1Size+nums2Size)%2 == 0)
+    {
+        return (GetMedian(nums1,nums1Size,nums2,nums2Size,(nums1Size+nums2Size)/2)+GetMedian(nums1,nums1Size,nums2,nums2Size,(nums1Size+nums2Size)/2+1))/2.0;
+    }
+    else
+        return GetMedian(nums1,nums1Size,nums2,nums2Size,(nums1Size+nums2Size)/2+1);
+}
 
-    if(nums1Size == 0 && nums2Size ==0)
-        return 0;
-    else if(nums1Size==0 && nums2Size !=0)
-    {
-        if(nums2Size%2==0)
-            return (nums2[(int)(nums2Size/2-1)]+nums2[(int)(nums2Size/2)])/2.0;
-        else
-            return nums2[(int)(nums2Size/2)];
-    }
-    else if(nums1Size != 0 && nums2Size ==0)
-    {
-        if(nums1Size%2==0)
-            return (nums1[(int)(nums1Size/2-1)]+nums1[(int)(nums1Size/2)])/2.0;
-        else
-            return nums1[(int)(nums1Size/2)];
-    }
-    else if(nums1Size == 1 && nums2Size ==1 && nums1[0] == nums2[0])
-    {
-        return nums1[0];
-    }
+int min(int a, int b){
+    if(a>=b)
+        return b;
+    else
+        return a;
+}
 
-
-    for(int i = 0;i<nums1Size;i++)
-    {
-        for(int j = 0; j<nums2Size;j++)
-        {
-            //printf("count1 is %d, count2 is %d\n", count1,count2);
-            if(nums2[j]>=nums1[i])
-                count1++;
-            else
-                count2++;
-            
-            if(nums1Size+nums2Size==3 && count1+count2==1)
-            {
-                if(count1>count2)
-                    return nums2[0];
-                else 
-                    return nums1[0];
-            }
-            else if(count1+count2==(nums1Size+nums2Size)/2-1)
-            {   
-                //printf("count1 is %d, count2 is %d\n", count1,count2);
-                break;
-            }
-        }
-        if(count1+count2==(nums1Size+nums2Size)/2-1)
-            break;
-    }
-   // printf("count1 is %d, count2 is %d\n", count1,count2);
-   // printf("data is %d, %d \n",nums1[count1],nums2[count2-1]);
-    return (nums1[count1]+nums2[count2])/2.0;
+int GetMedian(int* A, int m, int* B, int n, int k){
+    if(m>n)
+        return GetMedian(B,n,A,m,k);
+    if(m == 0)
+        return B[k-1];
+    if(k == 1)
+        return min(A[0],B[0]);
+    int a = min(k/2,m);
+    int b = k-a;
+    if(A[a-1]<=B[b-1])
+        return GetMedian(A+a,m-a,B,n,k-a);
+    return GetMedian(A,m,B+b,n-b,k-b);
+    
 }
 
 int main(int argc, char const *argv[])
