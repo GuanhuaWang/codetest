27bf90f6e5f7ae49f5cc1a2c556440a7b11e8bd7
GuanhuaWang <alexwgh333@gmail.com>
2017-11-26 17:36:42 -0800

    modify the coin change

--- leetcode/coinsChange.cpp
+++ leetcode/coinsChange.cpp
@@ -1,50 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <iostream>
-int* sortedCoins(int* coins, int coinsSize){
-    const int size = coinsSize;
-    int temp[size];
-    memset(temp,0,size*sizeof(int));
-    for(int i =0; i<coinsSize;i++){
-        temp[i]= coins[i];
-    }
-    for(int i = 0; i<size -1; i++){
-        for(int j =0;j<size - i -1;j++){
-            if(temp[j]>temp[j+1]){
-                int t = temp[j];
-                temp[j] = temp[j+1];
-                temp[j+1] = t;
+int coinChange(int* coins, int coinsSize, int amount) {  
+    int table[amount + 1];
+    table[0] = 0;
+    
+    for(int i = 1; i<=amount;i++)
+        table[i] = INT_MAX;
+    
+    for(int i = 1; i<=amount;i++){
+        for(int j = 0;j<coinsSize;j++)
+            if(coins[j]<=i){
+                int sub_res = table[i-coins[j]];
+                if(sub_res != INT_MAX && sub_res +1 <table[i])
+                    table[i] = sub_res + 1;
             }
-        }
     }
-    int* tem = temp;
-    return tem;
-}
-
-
-int coinChange(int* coins, int coinsSize, int amount) {
-    
-    int count = 0;
-    int* sorted = sortedCoins(coins, coinsSize);
-    
-    if (amount == 0)
-        return count;
-    if (amount < sorted[0])
-        return -1;
-    
-    for(int i =coinsSize-1;i>=0;i--){
-        if(amount>=sorted[i]){
-            amount-=sorted[i];
-            i++;
-            count++;
-        }
-        if(amount == 0)
-            return count;
-        else if(amount < sorted[0]) {
-            printf("amount is %d, sorrted 0 is %d\n", amount,sorted[0]);
-            return -2;
-        }
-    }
-    
-    return count;
+    return table[amount] == INT_MAX? -1: table[amount];
 }
 
 
